89de105e20fd22e3ad375574a78cf5ab374c6b7b
CARUSO AUGUSTO BONATELLI DA SILVA <caruso.silva@alunos.sc.senac.br>
2023-03-15 21:45:33 -0300

    .

--- src/Exemplos/Saudacao.java
+++ src/Exemplos/Saudacao.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Saudacao {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        System.out.println("Welcome to java programming");
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scan.nextLine();
-        System.out.println("you name is " + name);
+
+        System.out.print("Enter your message: ");
+        String massage = scan.nextLine();
+        System.out.println("You message is: " + massage);
+
     }
 }
