80a96789b8b5102f4fd083359925276dee0d25cc
CARUSO AUGUSTO BONATELLI DA SILVA <caruso.silva@alunos.sc.senac.br>
2023-03-22 19:54:36 -0300

    .

---
+++ src/Exercicios/IdadeEmDias.java
@@ -0,0 +1,23 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class IdadeEmDias {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite a idade em anos: ");
+        int anos = input.nextInt();
+
+        System.out.print("Digite a idade em meses: ");
+        int meses = input.nextInt();
+
+        System.out.print("Digite a idade em dias: ");
+        int dias = input.nextInt();
+
+        int idadeEmDias = anos * 365 + meses * 30 + dias;
+        System.out.println("A idade em dias é: " + idadeEmDias + " dias");
+
+        input.close();
+    }
+}
